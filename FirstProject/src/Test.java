157de7cf4822842a196548852834093096af4c11
alex <alex@DESKTOP-BP3U52R>
2019-05-16 22:18:44 +0200

    Added date

--- FirstProject/src/Test.java
+++ FirstProject/src/Test.java
@@ -1,13 +1,17 @@
+import java.util.Date;
 import java.util.Scanner;
 
 public class Test {
 
 	public static void main(String[] args) {
 		
+		Date date = new Date();
+		String today = date.toGMTString();
+		
 		Scanner scan = new Scanner(System.in);
 		System.out.println("Enter something: ");
 		String input = scan.next();
-		System.out.println(input);		
+		System.out.println(input +" Today is " + today);		
 		scan.close();
 		
 		
